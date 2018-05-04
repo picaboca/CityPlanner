a0dfc9bdee3517431e78446612684c18a41e83da
picaboca <davorhiti@gmail.com>
2018-05-04 08:46:06 +0200

    proba commita

--- app/src/main/java/hr/cityplanner/MainActivity.java
+++ app/src/main/java/hr/cityplanner/MainActivity.java
@@ -8,13 +8,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button butn = (Button)findViewById(R.id.paljenje_glavne_aktivnosti);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button butn = (Button)findViewById(R.id.paljenje_glavne_aktivnosti);
         butn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
